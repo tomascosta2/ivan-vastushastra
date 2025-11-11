6144fd15f2ec3ba99707567db972bcf4a3c3803b
tomas <tomascostapp@gmail.com>
2025-11-11 12:49:17 -0300

    Variante

--- src/pages/api/qualify.ts
+++ src/pages/api/qualify.ts
@@ -2,13 +2,23 @@ export const prerender = false;
 
 import type { APIRoute } from 'astro';
 
-export const POST: APIRoute = async ({ request }) => {
+export const POST: APIRoute = async ({ request, cookies }) => {
   try {
     const data = await request.json();
+    const variant = cookies.get("ab_vastu_title")?.value ?? "unknown";
+
+    console.log(variant)
+
+    const payload = {
+      ...data,
+      variant
+    }
 
     // TODO: enviar a Make/Zapier/DB si querés
-    await fetch('https://hook.us2.make.com/yni3jq2qvs9j8wxh3b2mo2f1limgf1vb', { method:'POST', headers:{'Content-Type':'application/json'}, body: JSON.stringify(data) });
+    const response = await fetch('https://hook.us2.make.com/yni3jq2qvs9j8wxh3b2mo2f1limgf1vb', { method:'POST', headers:{'Content-Type':'application/json'}, body: JSON.stringify(payload), credentials: "include" });
 
+    console.log(response)
+    
     return new Response(JSON.stringify({ ok: true }), {
       status: 200,
       headers: { "Content-Type": "application/json" },
